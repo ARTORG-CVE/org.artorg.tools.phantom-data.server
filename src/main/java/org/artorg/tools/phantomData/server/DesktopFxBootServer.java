3a96399f31b4ec8bb1ae50a5f0d3b671d1c80a05
Marc Hutzli <m.hutzli@hotmail.com>
2018-11-14 23:09:51 +0100

    fx startup improved

--- src/main/java/org/artorg/tools/phantomData/server/DesktopFxBootServer.java
+++ src/main/java/org/artorg/tools/phantomData/server/DesktopFxBootServer.java
@@ -1,5 +1,8 @@
 package org.artorg.tools.phantomData.server;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
@@ -11,48 +14,68 @@ import org.artorg.tools.phantomData.server.util.FxUtil;
 
 import huma.io.ConsoleDiverter;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class DesktopFxBootServer extends Application {
 	private FxStartupProgressContorller controller;
 	private String[] args;
-
-	public DesktopFxBootServer(String[] args) {
+	
+	public DesktopFxBootServer() {}
+	
+	public String[] getArgs() {
+		return args;
+	}
+	
+	public void setArgs(String[] args) {
 		this.args = args;
 	}
 	
 	public static void main(String[] args) {
-		new DesktopFxBootServer(args);
+		DesktopFxBootServer fxBooter = new DesktopFxBootServer();
+		fxBooter.setArgs(args);
+//		.launch(args);
 		Application.launch(args);
 	}
-
+	
 	@Override
 	public void start(Stage stage) throws Exception {
 		controller = new FxStartupProgressContorller(stage);
-		Pane pane =
-			FxUtil.loadFXML("fxml/Boot.fxml", controller, getClass());
+		Pane pane = FxUtil.loadFXML("fxml/Boot.fxml", controller, getClass());
 		Scene scene = new Scene(pane);
+		scene.getStylesheets().add(FxUtil.readCSSstylesheet("css/application.css"));
 		ServerBooter booter = createBooter(controller);
 //		scene.getStylesheets().add(FxUtil.readCSSstylesheet("css/application.css"));
 		
+		
+		
+		
+		stage.initStyle(StageStyle.UNDECORATED);
 		stage.getIcons().add(new Image("img/startup.png"));
 		stage.setScene(scene);
 		stage.setTitle("Phantom Database");
-		stage.setWidth(800);
-		stage.setHeight(500);
+		stage.setResizable(false);
+//		stage.setWidth(800);
+//		stage.setHeight(500);
+		
 		stage.show();
 		stage.requestFocus();
 		stage.toFront();
 		
-		booter.boot(args);
+		if (args == null) args = new String[] {};
+		Platform.runLater(() -> booter.boot(args));
+		
 	}
-
+	
 	public ServerBooter createBooter(FxStartupProgressContorller controller) {
 		return new ServerBooter() {
-
+			
 			@Override
 			public void boot(String[] args) {
 				try {
@@ -60,38 +83,54 @@ public class DesktopFxBootServer extends Application {
 					setExternalConfigOverridable(false);
 					setConsoleDiverter(new ConsoleDiverter());
 					setConsoleFrame(new SwingConsoleFrame());
-
+					
 					setStartupFrame(controller);
 					init();
 					prepareFileStructure();
 					DbFile.setFilesPath(getFilesPath());
-
+					
 					if (isDebugConsoleMode()) getConsoleFrame().setVisible(true);
 					if (!isConnected()) {
 						getStartupFrame().setnConsoleLines(39);
 						getStartupFrame().setTitle("Phantom Database");
 						getStartupFrame().setVisible(true);
 						getStartupFrame().setProgressing(true);
-						startSpringServer(args);
-						getStartupFrame().setVisible(false);
+						
+						Task<Void> task = new Task<Void>() {
+							@Override
+							protected Void call() throws Exception {
+								startSpringServer(args);
+								return null;
+							}	
+						};
+						Executors.newCachedThreadPool().execute(task);
+						while (!isConnected()) {
+							Thread.sleep(100);
+						}
+						
+//						getStartupFrame().setVisible(false);
 					} else {
 						JFrame frame = new JFrame();
 						JOptionPane.showMessageDialog(frame, "Server already started!");
 						frame.dispose();
 						System.exit(0);
 					}
-					getStartupFrame().dispose();
+					throw new IllegalArgumentException();
+//					getStartupFrame().dispose();
 				} catch (Exception e) {
 					getConsoleFrame().setTitle("Phantom Database - Exception thrown!");
 					setErrorOccured(true);
 					if (!super.handleException(e)) e.printStackTrace();
 				}
-				if (!getConsoleFrame().isErrorOccured() && !isErrorOccured()
-					&& !isDebugConsoleMode()) getConsoleFrame().setVisible(false);
-				else if (isRunnableJarExecution()) getConsoleFrame().setVisible(true);
+				if (!getConsoleFrame().isErrorOccured() && !isErrorOccured() && !isDebugConsoleMode())
+					getConsoleFrame().setVisible(false);
+				else 
+//					if (isRunnableJarExecution()) 
+						getConsoleFrame().setVisible(true);
+				
 			}
 		};
-
+		
 	}
-
+	
 }
--- src/main/java/org/artorg/tools/phantomData/server/boot/FxStartupProgressContorller.java
+++ src/main/java/org/artorg/tools/phantomData/server/boot/FxStartupProgressContorller.java
@@ -3,11 +3,14 @@ package org.artorg.tools.phantomData.server.boot;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import huma.io.ConsoleDiverter;
 import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
@@ -16,79 +19,93 @@ import javafx.stage.Stage;
 
 public class FxStartupProgressContorller extends StartupProgressFrame {
 	private final Stage stage;
-
+	private ExecutorService executorService = Executors.newCachedThreadPool();
+	
 	public FxStartupProgressContorller(Stage stage) {
 		this.stage = stage;
 	}
 	
-	
 	@FXML
 	private ResourceBundle resources;
-
+	
 	@FXML
 	private URL location;
-
+	
 	@FXML
 	private Label progressLabel;
-
+	
 	@FXML
 	private ProgressBar progressBar;
-
+	
 	@FXML
 	void close(MouseEvent event) {
 		stage.hide();
 		Platform.exit();
 		System.exit(0);
 	}
-
+	
 	@FXML
 	void initialize() {
 		assert progressLabel != null : "fx:id=\"progressLabel\" was not injected: check your FXML file 'Boot.fxml'.";
 		assert progressBar != null : "fx:id=\"progressBar\" was not injected: check your FXML file 'Boot.fxml'.";
+		
 	}
-
+	
 	private void updateStartupProgress(List<String> consoleLines, String newLine) {
 		if (isProgressing()) {
 			Pattern pattern = Pattern.compile("[^:]: (.*)");
 			if (consoleLines.size() > 0) {
-				if (consoleLines.size() < 10)
-					progressLabel.setText("Launching Spring Boot...");
-				else {
-					setProgress(getProgress() + 100.0 / getnConsoleLines());
-					progressBar.setProgress(getProgress());
-					Matcher m = pattern.matcher(newLine);
-					if (m.find()) progressLabel.setText(m.group(1));
-					else progressLabel.setText(newLine);
+				if (consoleLines.size() < 10) {
+					Platform.runLater(() -> {
+						progressLabel.setText("Launching Spring Boot...");
+					});
+				} else {
+					Task<Void> task = new Task<Void>() {
+
+						@Override
+						protected Void call() throws Exception {
+							setProgress(getProgress() + 100.0 / getnConsoleLines());
+							progressBar.setProgress(getProgress());
+							Matcher m = pattern.matcher(newLine);		
+							if (m.find()) progressLabel.setText(m.group(1));
+							else progressLabel.setText(newLine);
+							return null;
+						}
+						
+						
+					};
+					executorService.execute(task);
+					
 				}
 			}
 		}
 	}
-
+	
 	@Override
 	public void setConsoleDiverter(ConsoleDiverter consoleDiverter) {
 		super.setConsoleDiverter(consoleDiverter);
 		consoleDiverter.addOutLineConsumer(this::updateStartupProgress);
 	}
-
+	
 	@Override
 	public void setVisible(boolean b) {
 		if (b) stage.show();
 		else stage.hide();
 	}
-
+	
 	@Override
 	public void dispose() {
 		stage.hide();
 	}
-
+	
 	@Override
 	public void setTitle(String title) {
 		stage.setTitle(title);
 	}
-
+	
 	@Override
 	public Object getGraphic() {
 		return stage;
 	}
-
+	
 }
