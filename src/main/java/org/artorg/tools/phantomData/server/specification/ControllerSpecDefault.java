b6ebe59cd4e92db9e96b0b5406d11e0ed55cb39d
Marc Hutzli <m.hutzli@hotmail.com>
2018-11-26 16:37:37 +0100

    some UML diagrams

--- src/main/java/org/artorg/tools/phantomData/server/specification/ControllerSpecDefault.java
+++ src/main/java/org/artorg/tools/phantomData/server/specification/ControllerSpecDefault.java
@@ -5,5 +5,4 @@ import java.util.UUID;
 public abstract class ControllerSpecDefault<T extends Identifiable<UUID>,
 	I_SERVICE_TYPE extends IServiceDefault<T>>
 	extends ControllerSpecTemp<T, UUID, I_SERVICE_TYPE> {
-
 }
