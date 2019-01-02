beb5aa11dcb201dec28639ff6ee7823e7fe9c4c4
Marc Hutzli <m.hutzli@hotmail.com>
2019-01-02 23:03:40 +0100

    renamed generic type in DbPersistent

--- src/main/java/org/artorg/tools/phantomData/server/model/DbPersistentUUID.java
+++ src/main/java/org/artorg/tools/phantomData/server/model/DbPersistentUUID.java
@@ -2,7 +2,7 @@ package org.artorg.tools.phantomData.server.model;
 
 import java.util.UUID;
 
-public interface DbPersistentUUID<ITEM> extends DbPersistent<ITEM,UUID> {
+public interface DbPersistentUUID<T> extends DbPersistent<T,UUID> {
 	
 	default void setId(String value) {
 		setId(UUID.fromString(value));
